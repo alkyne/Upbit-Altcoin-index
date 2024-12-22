d80db672aa8c7e64ba946b27da6344e3cef9ac19
alkyne choi <a.com>
2024-12-22 22:14:46 +0900

    Modify check_settings.py

--- check_settings.py
+++ check_settings.py
@@ -1,18 +1,20 @@
 import json
 
+from settings import *
+
 def print_settings():
-    with open('settings.json', 'r') as file:
-        settings = json.load(file)
+    # with open('settings.json', 'r') as file:
+    #     settings = json.load(file)
 
-        num_of_alts = settings['num_of_alts']
-        input_krw = settings['input_krw']
-        exclude_pairs = settings['exclude_pairs']
+    #     num_of_alts = settings['num_of_alts']
+    #     input_krw = settings['input_krw']
+    #     exclude_pairs = settings['exclude_pairs']
 
-        print(f"num_of_alts: {num_of_alts}")
-        print(f"input_krw: {input_krw}")
-        krw_per_ticker = input_krw / num_of_alts
-        print(f"KRW per a ticker: {krw_per_ticker}")
-        print(f"exclude_pairs: {exclude_pairs}")
+    print(f"num_of_alts: {num_of_alts}")
+    print(f"input_krw: {input_krw}")
+    krw_per_ticker = floor(input_krw / num_of_alts)
+    print(f"KRW per a ticker: {krw_per_ticker}")
+    print(f"exclude_pairs: {exclude_pairs}")
 
 if __name__ == '__main__':
      print_settings()
